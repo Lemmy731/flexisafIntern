7afd4f71238af2fac4fae7fef66f8eff9a822317
Lemmy731 <lemmy.osiki@decagon.dev>
2025-01-23 12:06:34 +0100

    worked on book

---
+++ Book.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+
+    // Constructor
+    public Book(String title, String author, String ISBN) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+}
+
+class BookService {
+    private ArrayList<Book> arraylist = new ArrayList<Book>();
+
+    // add method
+    public void add(String title, String author, String ISBN) {
+        Book book = new Book(title, author, ISBN);
+        arraylist.add(book);  
+        System.out.println("Book added successfully: " + title);
+    }
+
+    // remove method
+    public void remove(String ISBN) {
+        for (Book book : arraylist) {
+            if (book.getISBN().equals(ISBN)) {
+                arraylist.remove(book);
+                System.out.println("Book removed with ISBN: " + ISBN);
+                return;
+            }
+        }
+        System.out.println("Book with ISBN " + ISBN + " removed.");
+    }
+}
