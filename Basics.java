c18166461cec4e86f1f60a65705192ba37f93096
Lemmy731 <lemmy.osiki@decagon.dev>
2025-01-30 13:17:43 +0100

    work on java basic

---
+++ Basics.java
@@ -0,0 +1,28 @@
+// understanding of basic java language with variables , fields , if else statement
+
+class Basics
+{
+    //fields
+    public String name;
+    public String email;
+    public String address;
+
+    public static void Calculate()
+    {
+        //variables
+        int price = 300;
+        int quantity = 9;
+
+        // if statement
+        if(quantity > 8)
+        {
+            price -= 50;
+        }
+
+        //else
+        else 
+        {
+            price += 10;
+        }
+    }
+}
--- Book.java
+++ Book.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 class Book {
+    
     private String title;
     private String author;
     private String ISBN;
